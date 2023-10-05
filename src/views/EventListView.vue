416e2555b39a647d5887ffd02cd1db31c4acd572
DAbranka <d.abranka@hotmail.com>
2023-10-05 16:49:36 +0200

    Fetching own api data with axios

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,44 +1,21 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import EventService from '@/services/EventService';
 
-let id = 0
+const events = ref(null)
 
-const events = ref([
-  {
-    id: id++,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28th, 2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer:  'Kat Laydee'
-  },
-  {
-    id: id++,
-    category: 'food',
-    title: 'Community Gardening',
-    description: 'Join us as we tend to the community edible plants.',
-    location: 'Flora City',
-    date: 'March 14th, 2022',
-    time: '10:00',
-    petsAllowed: true,
-    organizer:  'Fern Pollin'
-  },
-  {
-    id: id++,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'July 22nd, 2022',
-    time: '11:00',
-    petsAllowed: false,
-    organizer:  'Carey Wales'
-  },
-])
+// * Fetching data with Axios
+onMounted(() => {
+  EventService.getEvents()
+  .then((response) => {
+    events.value = response.data
+    console.log('event:', response.data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
 console.log(events.value)
 </script>
 
